82fa7b6541dc7b615e84ef182d4ea57d69809527
Hailin Li <hailin.li66@live.com>
2012-03-22 09:33:51 -0500

    Initial app

---
+++ app/controllers/rentals_controller.rb
@@ -0,0 +1,7 @@
+class RentalsController < ApplicationController
+  def home
+  end
+
+  def show
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RentalApp::Application.config.secret_token = 'e0574fc6a6387971c14254dd06ce27e689dcb9d5d4472715230d7dc12f9656b87cb1ccd71b06327cb807c7340c624c040791074ba9d8a038219b479fee773511'
